0e79965e16d0e5768cf03045b66c8f202638cd57
morimolymoly <morimolymoly@gmail.com>
2018-07-09 18:09:46 +0900

    redis support added

--- server/main.go
+++ server/main.go
@@ -5,16 +5,20 @@ import (
 	"log"
 	"net"
 
+	"github.com/go-redis/redis"
 	pb "github.com/morimolymoly/user-db-grpc/pb"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
 const (
+	redisAddress    = "user-db-grpc-redis:6379"
 	port            = ":8100"
 	success_message = "正常に登録されました"
 	// failed_message  = "なんかだめだわ……"
 )
+
+var redisClient *redis.Client
 
 // UserServiceServer ... implement UserServiceServer
 type UserServiceServer struct {
@@ -32,6 +36,16 @@ func (s *UserServiceServer) AddUser(ctx context.Context, req *pb.AddUserRequest)
 }
 
 func main() {
+	redisClient = redis.NewClient(&redis.Options{
+		Addr:     redisAddress,
+		Password: "",
+		DB:       0,
+	})
+	_, err := redisClient.Ping().Result()
+	if err != nil {
+		log.Fatalf("Failed to ping for redis: %v", err)
+	}
+
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
